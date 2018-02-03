6c1849c41b0117d022dc8a2d1475054adcdca30e
arraiz <mikeldiezgarcia@gmail.com>
2018-02-04 00:56:43 +0100

    added tone.js for sound & plotly.js for plots

--- index/index_renderer.js
+++ index/index_renderer.js
@@ -1,1 +1,17 @@
-console.log("WORKKKSSSS!!!")
+const tone = require('tone');
+TESTER = document.getElementById('tester');
+
+
+var osc = new tone.Oscillator(440, "sine").toMaster();
+console.log(osc.frequency)
+
+//graficar senos y cosenos
+
+
+Plotly.plot( TESTER, [{
+    x: [1, 2, 3, 4, 5],
+    y: [1, 2, 4, 8, 16] }], { 
+    margin: { t: 0 } } );
+
+
+
