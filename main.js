6a1f253a51cfe9867682cb79ddc138026964a57c
arraiz <mikeldiezgarcia@gmail.com>
2018-02-03 23:00:47 +0100

    last commit

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-const {app, BrowserWindow} = require('electron')
+const {
+  app,
+  BrowserWindow
+} = require('electron')
 const path = require('path')
 const url = require('url')
-locals={
-  hello:"12345",
-  hello2:"1234556",
-};
-const pug = require('electron-pug')({pretty: true}, locals);
-require('electron-reload')(__dirname)
+locals = null;
+const pug = require('electron-pug')({
+  pretty: true
+}, locals);
+require('electron-reload')(__dirname);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.pug'),
+    pathname: path.join(__dirname, '/index/index.pug'),
     protocol: 'file:',
-    slashes: true
-  }))
+    slashes: true,
+  }));
 
   // Open the DevTools.
   win.webContents.openDevTools()
--- renderer.js
+++
@@ -1,7 +0,0 @@
-let todosList = [];
-document.getElementById("submit_btn").addEventListener("click", function () {
-    //get value of input
-    let input = document.getElementById("todo_input").value;
-    todosList.push(input);
-    console.log(todosList);
-});
