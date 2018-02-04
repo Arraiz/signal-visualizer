1c8a14b58d9433a087dd7c6fca7472ec2c914f2a
arraiz <mikeldiezgarcia@gmail.com>
2018-02-04 16:04:30 +0100

    added sin&cos plots

---
+++ index/function_geneator.js
@@ -0,0 +1,7 @@
+//by arraiz
+
+//la idea de este modulo es que contenga las fucniones necesarias para generear las funciones basicas propuestas
+function generarSeno(){
+
+}
+function generarCoseno()
--- index/index_renderer.js
+++ index/index_renderer.js
@@ -1,17 +1,72 @@
 const tone = require('tone');
 TESTER = document.getElementById('tester');
 
+//funciones para el ploteo
+let xpos = [];
+let ypos = [];
+fs = 48000;
+ts = 1 / fs;
 
-var osc = new tone.Oscillator(440, "sine").toMaster();
-console.log(osc.frequency)
+//get the sginal type to plot
+document.getElementById('plot-button').addEventListener('click', function () {
+    let signalType = document.getElementById('signal-selector').value;
+    let freq = document.getElementById('freq-selector').value; 
+    ypos=[];
+    //choose the signal
+    switch (signalType) {
+        case 'sine':
+            generateSine(freq);
+            break;
+        case 'cosine':
+            generateCosine(freq);
+            break;
+        default:
+            break;
+    }
+    //plot the generated points
+    Plotly.purge(TESTER);
+    Plotly.newPlot(TESTER, [{
+        x: xpos,
+        y: ypos
+    }], {
+        margin: {
+            t: 0
+        }
+    });
+
+});
+
+
+
+
+
+
+/*oscilador*/
+//var osc = new tone.Oscillator(440, "sine").toMaster();
+///
+
+
+
+
+
+
+
+
 
 //graficar senos y cosenos
 
 
-Plotly.plot( TESTER, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }], { 
-    margin: { t: 0 } } );
 
 
-
+function generateSine(freq) {
+    for (let i = 0; i <= 1; i = i + ts) {
+        ypos.push(Math.sin(2 * Math.PI * i * freq));
+        xpos.push(i);
+    }
+}
+function generateCosine(freq) {
+    for (let i = 0; i <= 1; i = i + ts) {
+        ypos.push(Math.cos(2 * Math.PI * i * freq));
+        xpos.push(i);
+    }
+}
